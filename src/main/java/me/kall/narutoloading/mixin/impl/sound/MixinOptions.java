dc96760e5738c68b1e964e108492ecab9aa8130d
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-12-12 22:06:57 +0800

    bye bye vanilla music

---
+++ src/main/java/me/kall/narutoloading/mixin/impl/sound/MixinOptions.java
@@ -0,0 +1,17 @@
+package me.kall.narutoloading.mixin.impl.sound;
+
+import me.kall.narutoloading.NarutoLoadingClient;
+import net.minecraft.client.Options;
+import net.minecraft.sounds.SoundSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Options.class)
+public abstract class MixinOptions {
+    @Inject(method = "getSoundSourceVolume", at = @At("HEAD"), cancellable = true)
+    private void sound(SoundSource category, CallbackInfoReturnable<Float> cir) {
+        if (category.equals(SoundSource.MUSIC) && NarutoLoadingClient.RENDERER.isRunning()) cir.setReturnValue(0.0F);
+    }
+}
