1179f01200db308c6e3a69473cc649cb57e824b5
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-12-15 18:08:58 +0800

    Update FFmpeg.java

--- src/main/java/me/kall/narutoloading/data/FFmpeg.java
+++ src/main/java/me/kall/narutoloading/data/FFmpeg.java
@@ -7,12 +7,16 @@ import org.jetbrains.annotations.Nullable;
 import java.io.File;
 
 public final class FFmpeg {
-    private static final boolean isWindows = System.getProperty("os.name").toLowerCase().contains("win");
-    private static final File FFMPEG_NAME = get(isWindows ? "ffmpeg.exe" : "ffmpeg");
-    private static final File FFPROBE_NAME = get(isWindows ? "ffprobe.exe" : "ffprobe");
+    public static final @Nullable String FFMPEG;
+    public static final @Nullable String FFPROBE;
 
-    public static final @Nullable String FFMPEG = FFMPEG_NAME.exists() ? FFMPEG_NAME.getAbsolutePath() : null;
-    public static final @Nullable String FFPROBE = FFPROBE_NAME.exists() ? FFPROBE_NAME.getAbsolutePath() : null;
+    static {
+        boolean isWindows = System.getProperty("os.name").toLowerCase().contains("win");
+        File ffmpegFile = get(isWindows ? "ffmpeg.exe" : "ffmpeg");
+        File ffprobeFile = get(isWindows ? "ffprobe.exe" : "ffprobe");
+        FFMPEG = ffmpegFile.exists() ? ffmpegFile.getAbsolutePath() : null;
+        FFPROBE = ffprobeFile.exists() ? ffprobeFile.getAbsolutePath() : null;
+    }
 
     private static @NotNull File get(String fileName) {
         return FMLLoader.getGamePath().resolve("ffmpeg").resolve("bin").resolve(fileName).toFile();
