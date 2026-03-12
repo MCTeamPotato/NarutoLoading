5d99d14240d24dbe83c435f19d298b37aa2e6f9d
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2026-03-12 22:39:31 +0800

    Update ClientScreensRenderer.java

--- src/main/java/me/kall/narutoloading/inworld/core/ClientScreensRenderer.java
+++ src/main/java/me/kall/narutoloading/inworld/core/ClientScreensRenderer.java
@@ -266,7 +266,7 @@ public class ClientScreensRenderer {
             normalZ = -normalZ;
         }
 
-        if (!isFrontFacing) {
+        if (isFrontFacing) {
             leftBottomCornerX  += normalX;
             leftBottomCornerY += normalY;
             leftBottomCornerZ += normalZ;
