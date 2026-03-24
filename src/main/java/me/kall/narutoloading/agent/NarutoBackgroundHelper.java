7f903846a2888413dd1e7a8760a4618d2e285945
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2026-03-24 17:17:29 +0800

    Update NarutoBackgroundHelper.java

--- src/main/java/me/kall/narutoloading/agent/NarutoBackgroundHelper.java
+++ src/main/java/me/kall/narutoloading/agent/NarutoBackgroundHelper.java
@@ -32,7 +32,6 @@ public final class NarutoBackgroundHelper {
             "out vec2 vUv;",
             "void main() {",
             "    vUv         = aPos * 0.5 + 0.5;",
-            "    vUv.y       = 1.0 - vUv.y;",
             "    gl_Position = vec4(aPos, 0.0, 1.0);",
             "}"
     );
